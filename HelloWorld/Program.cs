8f489d350adca255ddc65dffdbe478a7da25a7f5
Pruthvi Tirmal <pruthvi.tirmal@wonderbotz.com>
2023-02-10 19:32:49 +0530

    Hello World example Added the C# console project for displaying the message "Hello World".

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloWorld
+{
+// created with "Do not use top-level statements" ticked !!
+// so, i got top-level statements.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
