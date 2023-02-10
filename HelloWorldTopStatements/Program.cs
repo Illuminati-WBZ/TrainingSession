86ae63dbc10c95b1faaf55775d94ed84c26c25a3
Pruthvi Tirmal <pruthvi.tirmal@wonderbotz.com>
2023-02-10 19:50:26 +0530

    Hello World Example With no top-level statements

---
+++ HelloWorldTopStatements/Program.cs
@@ -0,0 +1,5 @@
+﻿//  create with "Do not use top-level statements" no ticked !!
+// so, i got no top-level statements
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
