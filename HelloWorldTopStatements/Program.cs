fc999621e7b6ba305315d712a30821996aa44fb1
Pruthvi Tirmal <pruthvi.tirmal@wonderbotz.com>
2023-02-10 19:53:00 +0530

    Hello World Added some comments.

--- HelloWorldTopStatements/Program.cs
+++ HelloWorldTopStatements/Program.cs
@@ -1,5 +1,7 @@
 ﻿//  create with "Do not use top-level statements" no ticked !!
 // so, i got no top-level statements
+
+// notice no name-space, program class or main() method declarations. 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
